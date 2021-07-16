dbb89916b05e17d71df4bd300bb22460ad4e6bb4
Sirag Ahmed <ajj.siraj@gmail.com>
2021-07-16 08:35:21 +0200

    implemented nicer looking cross browser compatible scrollbar

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,18 +1,20 @@
 import { useState, useContext } from "react";
-import { Ctx, Types } from "../Context";
+import { Scrollbars } from "react-custom-scrollbars-2";
 
+import { Ctx, Types } from "../Context";
 import Brand from "./Brand";
 import NavItems from "./NavItems";
 import collapseIcon from "../svgs/sidenav-expand-icon.svg";
 
 //loading all nav svgs in the folder for ease of demonstration
-const reqSvgs = require.context ( '../svgs/navs', true, /\.svg$/ )
-const paths = reqSvgs.keys ()
+const reqSvgs = require.context("../svgs/navs", true, /\.svg$/);
+const paths = reqSvgs.keys();
 
-const svgs = paths.map( path => reqSvgs ( path ) )
+const svgs = paths.map((path) => reqSvgs(path));
 
 function SideNav() {
   let [showNav, updateNav] = useState(true);
+  let [windowHeight, setHeight] = useState(window.innerHeight);
   let width = showNav ? "w-1/6" : "w-1/12";
   let iconRotate = showNav ? "transform rotate-45" : "";
   const { dispatch } = useContext(Ctx);
@@ -56,18 +58,34 @@ function SideNav() {
     dispatch({ type: Types.SIDE_NAV_EXPANDED, payload: !showNav });
   }
 
+  function updateDimensions() {
+    setHeight(window.innerHeight);
+  }
+
   return (
-    <>
-      <div className={`${width} bg-gray-700 text-white h-screen transition-all duration-300 overflow-y-auto`}>
+    <div className={`${width} bg-gray-700 text-white transition-all duration-300 overflow-y-auto`}>
+      <Scrollbars
+        autoHide
+        autoHeight
+        autoHeightMin={windowHeight}
+        onUpdate={updateDimensions}
+        renderThumbVertical={(props) => (
+          <div {...props} className="bg-gray-400 rounded-md opacity-60" />
+        )}
+      >
         <div className="flex flex-col">
           <button className="bg-blue-400" onClick={handleUpdateNav}>
-            <img className={`${iconRotate} h-6 mx-auto transition-all duration-300`} src={collapseIcon} alt="collapse icon"/>
+            <img
+              className={`${iconRotate} h-6 mx-auto transition-all duration-300`}
+              src={collapseIcon}
+              alt="collapse icon"
+            />
           </button>
           <Brand />
           <NavItems>{sections}</NavItems>
         </div>
-      </div>
-    </>
+      </Scrollbars>
+    </div>
   );
 }
 
